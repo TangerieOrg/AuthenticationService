dbfc59e6efbf2a1b31e0386a72fc3d3932404643
Tangerie <josh.cavill01@gmail.com>
2022-09-18 13:42:42 +1000

    started

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("Hello World");
+import express from "express";
+
+const app = express();
+
+app.set('trust proxy', true)
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.json({})
+});
+
+app.listen(8080, () => {
+    console.log("Listening on", 8080);
+})
